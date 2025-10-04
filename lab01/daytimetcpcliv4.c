4bacdfa4b4709c9010c941e81b61b4e93e68ae80
0xF76 <jakub.bubak03@gmail.com>
2025-10-04 15:48:07 +0200

    cleanup code and add sending messages between server and client

--- lab01/daytimetcpcliv4.c
+++ lab01/daytimetcpcliv4.c
@@ -11,53 +11,85 @@
 #define MAXLINE 1024
 #define SA      struct sockaddr
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	int	sockfd, n, err;
 	struct sockaddr_in	servaddr;
-	char recvline[MAXLINE + 1];
+	char buff[MAXLINE + 1];
 
 	if (argc != 2){
 		fprintf(stderr, "usage: %s <IPaddress> \n", argv[0]);
 		return 1;
 	}
-	if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+	
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0){
 		fprintf(stderr,"socket error : %s\n", strerror(errno));
 		return 1;
 	}
 
-//sleep(3);
-	bzero(&servaddr, sizeof(servaddr));
+	memset(&servaddr, 0, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port   = htons(13);	/* daytime server */
-	if ( (err=inet_pton(AF_INET, argv[1], &servaddr.sin_addr.s_addr)) == -1){
+
+
+	err = inet_pton(AF_INET, argv[1], &servaddr.sin_addr.s_addr);
+	if(err == -1) {
 		fprintf(stderr,"ERROR: inet_pton error for %s : %s \n", argv[1], strerror(errno));
 		return 1;
-	}else if(err == 0){
+	} else if(err == 0) {
 		fprintf(stderr,"ERROR: Invalid address family \n");
 		return 1;
 	}
-	
-	if (connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0){
+
+	err = connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+	if (err < 0){
 		fprintf(stderr,"connect error : %s \n", strerror(errno));
 		return 1;
 	}
 
-	while ( (n = read(sockfd, recvline, MAXLINE)) > 0) {
-		recvline[n] = 0;	/* null terminate */
-		if (fputs(recvline, stdout) == EOF){
+	printf("Connected to %s\n", argv[1]);
+
+	n = read(sockfd, buff, MAXLINE);
+	if (n < 0) {
+		fprintf(stderr,"read error : %s\n", strerror(errno));
+		return 1;
+	} else if(n > 0) {
+		buff[n] = 0;
+		if (fputs(buff, stdout) == EOF){
 			fprintf(stderr,"fputs error : %s\n", strerror(errno));
 			return 1;
 		}
 	}
 	
-	if (n < 0)
-		fprintf(stderr,"read error : %s\n", strerror(errno));
+	while(1) {
+		fgets(buff, MAXLINE, stdin);
+		buff[strcspn(buff, "\n")] = 0; // remove newline character
+		
+		err = write(sockfd, buff, strlen(buff));
+		if(err < 0) {
+			fprintf(stderr,"write error : %s\n", strerror(errno));
+		}
 
-	fprintf(stderr,"OK\n");
-	fflush(stdout);
-	fgetc(stdin);	
-//sleep(10);
-	exit(0);
+		if (strncmp(buff, "exit", 4) == 0) {
+			printf("Exiting...\n");
+			break;
+		}
+
+		n = read(sockfd, buff, MAXLINE);
+		if (n < 0) {
+			fprintf(stderr,"read error : %s\n", strerror(errno));
+			return 1;
+		} else if(n > 0) {
+			buff[n] = 0;
+			printf("Server: ");
+			if (fputs(buff, stdout) == EOF){
+				fprintf(stderr,"fputs error : %s\n", strerror(errno));
+				return 1;
+			}
+		}	
+	}
+
+	close(sockfd);
+	return 0;
 }
--- lab01/daytimetcpsrvv4.c
+++ lab01/daytimetcpsrvv4.c
@@ -15,63 +15,85 @@
 
 #define LISTENQ 2
 
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-    int				listenfd, connfd;
-    socklen_t			len;
-    char				buff[MAXLINE], str[INET6_ADDRSTRLEN+1];
-    time_t				ticks;
+    int listenfd, connfd, err, n;
+    socklen_t len;
+    char buff[MAXLINE], str[INET6_ADDRSTRLEN+1];
+    time_t ticks;
     struct sockaddr_in servaddr, cliaddr;
 
-    if ( (listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd < 0) {
         fprintf(stderr,"socket error : %s\n", strerror(errno));
         return 1;
     }
 
-//sleep(3);
-    bzero(&servaddr, sizeof(servaddr));
+    memset(&servaddr, 0, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr  = htonl(INADDR_ANY);
     servaddr.sin_port   = htons(13);	/* daytime server */
 
-    if ( bind( listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0){
+    err = bind(listenfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+    if (err < 0){
         fprintf(stderr,"bind error : %s\n", strerror(errno));
         return 1;
     }
-//sleep(3);
-    if ( listen(listenfd, LISTENQ) < 0){
+
+    err = listen(listenfd, LISTENQ);
+    if (err < 0){
         fprintf(stderr,"listen error : %s\n", strerror(errno));
         return 1;
     }
-//sleep(3);
-    for ( ; ; ) {
+
+    while(1) {
         len = sizeof(cliaddr);
-            if ( (connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len)) < 0){
-                fprintf(stderr,"accept error : %s\n", strerror(errno));
-                continue;
-        	}
-//sleep(3);
-        bzero(str, sizeof(str));
-        inet_ntop(AF_INET, (struct sockaddr  *) &cliaddr.sin_addr.s_addr,  str, sizeof(str));
+        connfd = accept(listenfd, (struct sockaddr *) &cliaddr, &len);
+        if (connfd < 0){
+            fprintf(stderr,"accept error : %s\n", strerror(errno));
+            continue;
+        }
+        memset(str, 0, sizeof(str));
+        inet_ntop(AF_INET, (struct sockaddr *) &cliaddr.sin_addr.s_addr,  str, sizeof(str));
         printf("Connection from %s\n", str);
+        
+        //send date and time
+        ticks = time(NULL);
+        snprintf(buff, sizeof(buff), "Welcome to server, the time is: %.24s\r\n", ctime(&ticks));
+        
+        err = write(connfd, buff, strlen(buff));
+        if(err < 0) {
+            fprintf(stderr,"write error : %s\n", strerror(errno));
+        }
 
-	while(n = read(connfd, buff, MAXLINE)) {
-		recvline[n] = 0;
-		err = fputs(buff, stdout);
-		if(err == EOF) {
-			fprintf(stderr, "fputs error: %s\n", strerror(errno));
-			return 1;
-		}
-		fgets(buff, MAXLINE, stdin);
-		err = write
+        while(1) {
+            memset(buff, 0, sizeof(buff));
+            n = read(connfd, buff, MAXLINE);
+            if(n < 0) {
+                fprintf(stderr, "read error: %s\n", strerror(errno));
+                break;
+            }
+            printf("Client: %s\n", buff);
+            if(strncmp(buff, "exit", 4) == 0) {
+                printf("Client disconnected.\n");
+                break;
+            }
+            
+            fgets(buff, MAXLINE, stdin);
+            
+            err = write(connfd, buff, strlen(buff));
+            if(err < 0) {
+                fprintf(stderr,"write error : %s\n", strerror(errno));
+                break;
+            }            
+        }
 
-	}
+        close(connfd);
 
-        //ticks = time(NULL);
-        //snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
-        //if( write(connfd, buff, strlen(buff))< 0 )
-       	//    fprintf(stderr,"write error : %s\n", strerror(errno));
-        //close(connfd);
-	}
+    }
+
+    close(listenfd);
+    return 0;
+	
 }
